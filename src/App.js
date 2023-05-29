ba716780c4cc553bb5c911cf1fde0aaf4dd6dc7c
Lakshmip13 <lakshmi97kal@gmail.com>
2023-05-29 12:38:28 +0530

    added comment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
  import logo from './logo.svg';
  import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Addmovie from "./components/Addmovie";
@@ -39,6 +16,8 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
+        
+        {/* App */}
         <Route path="/" element={ <Home/>}/>
       <Route path="/add" element={<Addmovie/>}/>
       <Route path='/moviedetails/:id' element={<Moviedetails/>}/>
